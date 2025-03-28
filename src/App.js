636426e29bbe5eac0f1dd682a0b1df320e4a4e11
Rinkoo-ctrl <rinkookandpal2000@gmail.com>
2025-03-28 07:50:26 +0530

    Initial React setup for todo-list-app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Todo List</h1>
+      {/* Yahan baad me todo ka code aayega */}
+    </div>
+  );
+}
+
+export default App;
